1966a61f3b919e5d91635119b44e05d0311010bd
Matheus Eickhoff <127603510+Matheus-Ei@users.noreply.github.com>
2023-06-06 21:35:37 -0300

    Create getDatabaseYahooFinances.py

---
+++ getDatabaseYahooFinances.py
@@ -0,0 +1,18 @@
+import yfinance as yf
+
+# Definir o ticker do ativo
+ticker = "AAPL"  # Substitua pelo ticker do ativo desejado
+
+# Definir o intervalo de datas
+start_date = "2021-01-01"
+end_date = "2022-01-01"
+
+# Obter os dados do Yahoo Finance
+data = yf.download(ticker, start=start_date, end=end_date)
+
+# Selecionar apenas as colunas desejadas
+selected_data = data[["Close", "Volume"]].copy()
+selected_data.reset_index(inplace=True)
+
+# Salvar os dados em um arquivo CSV
+selected_data.to_csv("dados.csv", index=False)
